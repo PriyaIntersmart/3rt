cb3075321809720cdc58ed55ce830f55b736274d
PriyaIntersmart <priya@intersmart.in>
2024-04-30 12:23:11 +0530

    delete the backend module

--- Modules/Backend/resources/views/dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('backend::layouts.app')
-@section('title', 'Dashboard')
-@section('content')
-<h1>WELCOME,{{ Auth::user()->name }}</h1>
-
-
-@endsection
-
--- Modules/Backend/resources/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('backend::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('backend.name') !!}</p>
-@endsection
