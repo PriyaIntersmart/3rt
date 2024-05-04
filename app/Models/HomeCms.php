a07409b41a67da3a4d6465029a6e861b94052892
PriyaIntersmart <priya@intersmart.in>
2024-05-04 11:52:27 +0530

    home cms image trait added

--- app/Models/HomeCms.php
+++ app/Models/HomeCms.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\ImageTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 
 class HomeCms extends Model
 {
-    use HasFactory;
+    use HasFactory,ImageTrait;
 
 
     protected $fileDirectory = 'Home-cms';
